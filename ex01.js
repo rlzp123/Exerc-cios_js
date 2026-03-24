ebcc717acaa6137f5b19ee98e146fc858b0bb9f1
Ryan <ryan.zamboni.1b@gmail.com>
2026-03-24 15:06:43 -0300

    Add files via upload

---
+++ ex01.js
@@ -0,0 +1,5 @@
+let cosmoBase = 100
+cosmoBase *= 5
+cosmoBase -= 50
+
+console.log(cosmoBase)
---
+++ ex02.js
@@ -0,0 +1,6 @@
+let armadura = false;
+let cosmoShiryu = 8000;
+let cosmoInimigo = 7500;
+let vitoria = !armadura && cosmoShiryu > cosmoInimigo;
+
+console.log(vitoria)
